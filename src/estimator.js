ac64db0c66132abc10c32f37ad9cf559028b4d60
obafemi oderanti <thevetdoctor.gmail.com>
2020-04-11 05:18:25 +0100

    update on code audit

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,3 @@
-const inputData = {
-    region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-   };
-
 const outputData = {
     data: {}, // the input data you got
     impact: {}, // your best case estimation
