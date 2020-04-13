03dcbcde3377789f57315722bc47cc9868349ce9
obafemi oderanti <thevetdoctor.gmail.com>
2020-04-13 21:49:53 +0100

    challenge 1 audit check

---
+++ src/testing.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+const data = [
+  {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 3,
+      avgDailyIncomePopulation: 0.68
+    },
+    reportedCases: 1030,
+    population: 6231388,
+    totalHospitalBeds: 243398,
+    timeToElapse: 2,
+    periodType: 'months'
+  },
+  {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 2,
+      avgDailyIncomePopulation: 0.61
+    },
+    reportedCases: 580,
+    population: 9683987,
+    totalHospitalBeds: 108936,
+    timeToElapse: 12,
+    periodType: 'weeks'
+  },
+  {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 3,
+      avgDailyIncomePopulation: 0.66
+    },
+    reportedCases: 1099,
+    population: 4886599,
+    totalHospitalBeds: 205838,
+    timeToElapse: 20,
+    periodType: 'days'
+  }];
+console.log(data);
