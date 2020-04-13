2742b50176e51630fa1301e3638c1bba41823a2e
obafemi oderanti <thevetdoctor.gmail.com>
2020-04-13 21:59:22 +0100

    exclude chall 2 and 3

--- src/estimator.js
+++ src/estimator.js
@@ -23,56 +23,66 @@ const covid19ImpactEstimator = (data) => {
 
   // challenge 2
   // 15% of infectionsByRequestedTime for both impact & severeImpact
-  const severeCasesByRequestedTimeFactor = 0.15;
-  const severeCasesByRequestedTimeImpact = severeCasesByRequestedTimeFactor * infectionsByRequestedTimeImpact;
-  const severeCasesByRequestedTimeSevereImpact = severeCasesByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact;
+  // const severeCasesByRequestedTimeFactor = 0.15;
+  // const severeCasesByRequestedTimeImpact = severeCasesByRequestedTimeFactor * infectionsByRequestedTimeImpact;
+  // const severeCasesByRequestedTimeSevereImpact = severeCasesByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact;
 
-  const { totalHospitalBeds } = data;
+  // const { totalHospitalBeds } = data;
 
-  const bedAvailability = 0.35; // 35% of totalHospitalBeds
-  const hospitalBedsByRequestedTime = Math.floor(bedAvailability * totalHospitalBeds);
-  const hospitalBedsByRequestedTimeImpact = hospitalBedsByRequestedTime - severeCasesByRequestedTimeImpact;
-  const hospitalBedsByRequestedTimeSevereImpact = hospitalBedsByRequestedTime - severeCasesByRequestedTimeSevereImpact;
+  // const bedAvailability = 0.35; // 35% of totalHospitalBeds
+  // const hospitalBedsByRequestedTime = Math.floor(bedAvailability * totalHospitalBeds);
+  // const hospitalBedsByRequestedTimeImpact = hospitalBedsByRequestedTime - severeCasesByRequestedTimeImpact;
+  // const hospitalBedsByRequestedTimeSevereImpact = hospitalBedsByRequestedTime - severeCasesByRequestedTimeSevereImpact;
 
 
-  // challenge 3
+  // // challenge 3
 
-  // 5% of infectionsByRequestedTime for both impact & severeImpact
-  const casesForICUByRequestedTimeFactor = 0.05;
+  // // 5% of infectionsByRequestedTime for both impact & severeImpact
+  // const casesForICUByRequestedTimeFactor = 0.05;
 
-  // 2% of infectionsByRequestedTime for both impact & severeImpact
-  const casesForVentilatorsByRequestedTimeFactor = 0.02;
+  // // 2% of infectionsByRequestedTime for both impact & severeImpact
+  // const casesForVentilatorsByRequestedTimeFactor = 0.02;
 
-  const casesForICUByRequestedTimeImpact = Math.floor(casesForICUByRequestedTimeFactor * infectionsByRequestedTimeImpact);
-  const casesForICUByRequestedTimeSevereImpact = Math.floor(casesForICUByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact);
+  // const casesForICUByRequestedTimeImpact = Math.floor(casesForICUByRequestedTimeFactor * infectionsByRequestedTimeImpact);
+  // const casesForICUByRequestedTimeSevereImpact = Math.floor(casesForICUByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact);
 
-  const casesForVentilatorsByRequestedTimeImpact = Math.floor(casesForVentilatorsByRequestedTimeFactor * infectionsByRequestedTimeImpact);
-  const casesForVentilatorsByRequestedTimeSevereImpact = Math.floor(casesForVentilatorsByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact);
+  // const casesForVentilatorsByRequestedTimeImpact = Math.floor(casesForVentilatorsByRequestedTimeFactor * infectionsByRequestedTimeImpact);
+  // const casesForVentilatorsByRequestedTimeSevereImpact = Math.floor(casesForVentilatorsByRequestedTimeFactor * infectionsByRequestedTimeSevereImpact);
 
-  const { avgDailyIncomeInUSD } = data.region;
-  const { avgDailyIncomePopulation } = data.region;
+  // const { avgDailyIncomeInUSD } = data.region;
+  // const { avgDailyIncomePopulation } = data.region;
 
-  const dollarsInFlightImpact = Math.floor((infectionsByRequestedTimeImpact * avgDailyIncomePopulation * avgDailyIncomeInUSD) / requestedTime);
-  const dollarsInFlightSevereImpact = Math.floor((infectionsByRequestedTimeSevereImpact * avgDailyIncomePopulation * avgDailyIncomeInUSD) / requestedTime);
+  // const dollarsInFlightImpact = Math.floor((infectionsByRequestedTimeImpact * avgDailyIncomePopulation * avgDailyIncomeInUSD) / requestedTime);
+  // const dollarsInFlightSevereImpact = Math.floor((infectionsByRequestedTimeSevereImpact * avgDailyIncomePopulation * avgDailyIncomeInUSD) / requestedTime);
+
+  // const estimate = {
+  //   impact: {
+  //     currentlyInfected: currentlyInfectedImpact,
+  //     infectionsByRequestedTime: infectionsByRequestedTimeImpact,
+  //     severeCasesByRequestedTime: severeCasesByRequestedTimeImpact,
+  //     hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeImpact,
+  //     casesForICUByRequestedTime: casesForICUByRequestedTimeImpact,
+  //     casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeImpact,
+  //     dollarsInFlight: dollarsInFlightImpact
+  //   },
+  //   severeImpact: {
+  //     currentlyInfected: currentlyInfectedSevereImpact,
+  //     infectionsByRequestedTime: infectionsByRequestedTimeSevereImpact,
+  //     severeCasesByRequestedTime: severeCasesByRequestedTimeSevereImpact,
+  //     hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeSevereImpact,
+  //     casesForICUByRequestedTime: casesForICUByRequestedTimeSevereImpact,
+  //     casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevereImpact,
+  //     dollarsInFlight: dollarsInFlightSevereImpact
+  //   }
 
   const estimate = {
     impact: {
       currentlyInfected: currentlyInfectedImpact,
-      infectionsByRequestedTime: infectionsByRequestedTimeImpact,
-      severeCasesByRequestedTime: severeCasesByRequestedTimeImpact,
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeImpact,
-      casesForICUByRequestedTime: casesForICUByRequestedTimeImpact,
-      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeImpact,
-      dollarsInFlight: dollarsInFlightImpact
+      infectionsByRequestedTime: infectionsByRequestedTimeImpact
     },
     severeImpact: {
       currentlyInfected: currentlyInfectedSevereImpact,
-      infectionsByRequestedTime: infectionsByRequestedTimeSevereImpact,
-      severeCasesByRequestedTime: severeCasesByRequestedTimeSevereImpact,
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeSevereImpact,
-      casesForICUByRequestedTime: casesForICUByRequestedTimeSevereImpact,
-      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevereImpact,
-      dollarsInFlight: dollarsInFlightSevereImpact
+      infectionsByRequestedTime: infectionsByRequestedTimeSevereImpact
     }
   };
 
